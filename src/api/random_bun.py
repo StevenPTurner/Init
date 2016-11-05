81455abe6824becf6d0d05b0412a15db649bb38a
StevenPTurner <spt10pd@msn.com>
2016-11-05 23:08:14 +0000

    Fixed bug, was calling wrong method to generate random number

--- src/api/random_bun.py
+++ src/api/random_bun.py
@@ -1,7 +1,7 @@
 import random
 
 def random_number(number_of_buns):
-    print random.random(1, number_of_buns)
+    print random.randint(1, number_of_buns)
 
 def main(number_of_buns):
     random_number(number_of_buns)
