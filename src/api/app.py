d5006c69877a6ba2bfe2a7b1dac4c6fa9e9776ae
StevenPTurner <spt10pd@msn.com>
2016-11-05 20:28:06 +0000

    Added new route with templates

--- src/api/app.py
+++ src/api/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return render_remplate(../'index.html')
+
+@app.route('/cakes')
+def cakes():
+    return 'Cake!'
 
 if __name__== '__main__':
     app.run(debug=True, host='0.0.0.0')
